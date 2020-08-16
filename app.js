df7ba80fb6337843a3ef41a8f0d208d48dad4f19
simpibobo <69766184+simpibobo@users.noreply.github.com>
2020-08-16 21:24:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+var const firebaseConfig = {
+  apiKey: "AIzaSyBoxep75vpcltm4gEngn55rtGLiKjX3jKk",
+  authDomain: "test123-16d34.firebaseapp.com",
+  databaseURL: "https://test123-16d34.firebaseio.com",
+  projectId: "test123-16d34",
+  storageBucket: "test123-16d34.appspot.com",
+  messagingSenderId: "579375515106",
+  appId: "1:579375515106:web:56c81fc88a82d8b8aea5bb",
+  measurementId: "G-27P71J0GEP"
+};
 };
 
      
